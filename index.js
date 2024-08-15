e1be6254f0f0a1222006324f2fbd97307d0fd2a1
aryangurau <aryangurau143@gmail.com>
2024-08-15 08:43:40 +0545

    Application setup completion

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = Number(process.env.PORT) || 4646;
+const indexRouter = require("./routes");
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("database error", err);
+  });
+
+app.use(express.json());
+app.use("/resources", express.static("public"));
+app.use(morgan("tiny"));
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  const errMsg = err ? err.toString() : "something went wrong";
+  res.json({ data: "", msg: errMsg });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
